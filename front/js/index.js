0585b42f1ffc4a3098fbaf597a6014bffc2da651
MDI <amaloumehdi6@gmail.com>
2022-07-13 18:29:02 +0200

    Modif index.js

--- front/js/index.js
+++ front/js/index.js
@@ -8,71 +8,21 @@ const items = async function () {
     itemsData = promise;
   });
 }
+/*const getOneProduct.split(",").join("");*/
 
-const getOneProduct =  async () => {
+const getProducts =  async () => {
   await items();
   console.log(itemsData)
-  document.getElementById("items").innerHTML = itemsData.map((item) =>` <a href="./product.html?id=${item._id}">
-                                                                          <article>
-                                                                            <img src="${item.imageUrl}" alt="${item.altTxt}">
-                                                                              <h3 class="productName">${item.name}</h3>
-                                                                              <p class="productDescription">${item.description}</p>
-                                                                          </article>
-                                                                        </a> `);
 
-var oneProduct = document.querySelectorAll("#items")                                                                        
-  console.log(oneProduct) 
-  
-  getOneProduct.forEach((oneProduct) => 
-  oneProduct.addEventListener("click", () => {
-    console.log(oneProduct)
-
-    window.location = ` product.html?${oneProduct.id} ` ;
-  }),
-  ); 
-};
-
-getOneProduct ()
-
-
-
-
-
-
-
-/*
-var links = document.querySelectorAll('a.products')
-for ( var i = 0 ; i < links.strenght ; i++ ) {
-  var link = links[i]
-  link.addEventListener('click') , function(event) {
-      event.defaultPrevented()
-      console.log(i)
-  }
+  document.getElementById("items").innerHTML = itemsData.map((item) =>
+  `<a href="./product.html?id=${item._id}">
+    <article>
+      <img src="${item.imageUrl}" alt="${item.altTxt}">
+        <h3 class="productName">${item.name}</h3>
+        <p class="productDescription">${item.description}</p>
+    </article>
+  </a>`
+);
 }
-*/
 
-/**
- * Gestion des articles en objet, gère le tri par date et le formatage des dates pour affichage
- 
-class productItems{
-  constructor(listProducts){
-      this.listProducts = listProducts;
-  }
-
-  sort(){
-      return this.listProducts.sort((a, b) => {
-          return () < )?1:-1;
-      });
-  }
-}
-*/
-
-
-/*  Je veux afficher les données de l'api sur la page html 
-    Il faut utiliser JSON, fecth et une boucle FOR
-
-*/
-/* 
-const products = document.getElementById ('items')
-console.log('Voici les produits', items)
-*/
+getProducts()
